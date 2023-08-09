68f477273f427150a1da9febb4b96aec1751581a
Rael <allice2283006@gmail.com>
2023-08-09 16:57:40 +0900

    [D4] Title: 정사각형 방, Time: 333 ms, Memory: 29,224 KB -BaekjoonHub

--- SWEA/D4/1861. 정사각형 방/정사각형 방.java
+++ SWEA/D4/1861. 정사각형 방/정사각형 방.java
@@ -4,14 +4,13 @@ import java.util.ArrayDeque;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-//BFS로 풀기
+//DFS로 풀기
 public class Solution {
 	static int T, N, NO, COUNT;		//NO: 시작 방 번호, COUNT: 가장 긴 이동 횟수 
 	static int[][] room;
 
 	static int[] dx = {0,0,-1,1};	//상하좌우
 	static int[] dy = {-1,1,0,0};	//상하좌우
-	static Queue<Node> queue = new ArrayDeque<>();
 	
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -37,8 +36,7 @@ public class Solution {
 			//모든 방에서 다 가본다. (완탐-bfs)
 			for(int i=0; i<N; i++) {
 				for(int j=0; j<N; j++){
-					queue.offer(new Node(i,j,room[i][j],1));
-					bfs();
+					dfs(i, j, room[i][j], 1);
 				}
 			}
 			sb.append("#").append(t).append(" ").append(NO).append(" ").append(COUNT);
@@ -46,40 +44,25 @@ public class Solution {
 			System.out.println(sb);
 		}
 	}
-	static void bfs() {
-		while(!queue.isEmpty()) {
-			Node node = queue.poll();
-			
-			//최대인가?
-			if(node.cnt > COUNT) {
-				COUNT = node.cnt;
-				NO = node.no;
-			}else if(node.cnt==COUNT) {
-				NO = node.no < NO ? node.no : NO;	//Math.min()과 동일한 동작
-			}
-			
-			//사방 탐색
-			for(int d=0; d<4; d++) {
-				int ny = node.y + dy[d];
-				int nx = node.x + dx[d];
-				
-				//유효범위 확인
-				if(ny < 0 || nx < 0 || ny >= N || nx >= N || 
-						room[ny][nx] != room[node.y][node.x]+1) continue;
-				queue.offer(new Node(ny, nx, node.no, node.cnt+1));
-				break; //조건에 맞는 방은 하나.
-			}
+	static void dfs(int y, int x, int no, int cnt) {
+		//최대인가?
+		if(cnt > COUNT) {
+			COUNT = cnt;
+			NO = no;
+		}else if(cnt==COUNT) {
+			NO = no < NO ? no : NO;	//Math.min()과 동일한 동작
 		}
-	}
-	
-	static class Node{
-		int y,x,no,cnt;
 		
-		Node(int y, int x, int no, int cnt){
-			this.y=y;
-			this.x=x;
-			this.no=no;
-			this.cnt=cnt;
+		//사방 탐색
+		for(int d=0; d<4; d++) {
+			int ny = y + dy[d];
+			int nx = x + dx[d];
+			
+			//유효범위 확인
+			if(ny < 0 || nx < 0 || ny >= N || nx >= N || 
+					room[ny][nx] != room[y][x]+1) continue;
+			dfs(ny, nx, no, cnt+1);
+			break; //조건에 맞는 방은 하나.
 		}
 	}
 }
