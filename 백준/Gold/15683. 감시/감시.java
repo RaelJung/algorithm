1c1c5e60a0f78b00c85a253870411ea46d6d94a8
Rael <allice2283006@gmail.com>
2023-08-22 17:48:49 +0900

    [Gold IV] Title: 감시, Time: 312 ms, Memory: 90248 KB -BaekjoonHub

--- 백준/Gold/15683. 감시/감시.java
+++ 백준/Gold/15683. 감시/감시.java
@@ -1,103 +1,95 @@
-// BOJ - 감시 (15683번)
-// DFS(조합)
-
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Main {
-	
-	public static int[] dx = {-1, 1, 0, 0};
-	public static int[] dy = {0, 0, -1, 1};
+	static int N, M, ans;
+	static int[][] map;
+	static ArrayList<Pos> cam;
+	//상하좌우
+	static int dy[] = {0,0,-1,1};
+	static int dx[] = {-1,1,0,0};
 	
 	public static int[][][] mode = {{{0}}, {{0}, {1}, {2}, {3}}, {{2, 3}, {0, 1}},
 			{{0, 3}, {1, 3}, {1, 2}, {0, 2}},
 			{{0, 2, 3}, {0, 1, 3}, {1, 2, 3}, {0, 1, 2}},
 			{{0, 1, 2, 3}}};
-	public static ArrayList<Node> cctv;
-	public static class Node {
-		int x;
-		int y;
-		int type;
-		public Node(int x, int y, int type) {
-			this.x= x;
-			this.y= y;
-			this.type = type;
-		}
-	}
-	public static int n, m, ans;
-	public static int[][] map;	
-	public static void main(String[] args) throws IOException {
-		
+	
+	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
-		cctv = new ArrayList<>();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		map = new int[N][M];
+		cam = new ArrayList<>();
 		int zero_cnt = 0;
-		int[][] map = new int[n][m];
-		for(int i=0;i<n;i++) {
-			st = new StringTokenizer(br.readLine(), " ");
-			for(int j=0;j<m;j++) {
+		
+		//입력
+		for(int i=0; i<N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j=0; j<M; j++) {
 				map[i][j] = Integer.parseInt(st.nextToken());
-				if(map[i][j] >= 1 && map[i][j] <= 5){
-					cctv.add(new Node(i, j, map[i][j]));
-				} else if(map[i][j] == 0) zero_cnt++;
+				
+				if(map[i][j] >=1 && map[i][j] <= 5) {
+					cam.add(new Pos(map[i][j], i, j));
+				}
+				else if(map[i][j] == 0) zero_cnt++;
 			}
 		}
+		
 		ans = zero_cnt;
-		combination(0, cctv.size(), map);
+		
+		comb(0, cam.size(), map);
+		
 		System.out.println(ans);
 	}
 	
-	
-	public static void combination(int depth, int r, int[][] map) {
-		if(depth == r) {
+	public static void comb(int depth, int size, int[][] map) {
+		//기저조건
+		if(depth == size) {
 			int cnt = check(map);
 			ans = Math.min(ans, cnt);
 			return;
 		}
-		int cctv_type = cctv.get(depth).type;
-		int x = cctv.get(depth).x;
-		int y = cctv.get(depth).y;
 		
+		int type = cam.get(depth).type;
+		int x = cam.get(depth).x;
+		int y = cam.get(depth).y;
 		
-		for(int i=0;i<mode[cctv_type].length;i++) {
-			int[][] map_copy = new int[n][m];
-			for(int k=0;k<n;k++) {
+		for(int i=0; i<mode[type].length; i++) {
+			int[][] map_copy = new int[N][M];
+			for(int k=0; k<N; k++) {
 				map_copy[k] = map[k].clone();
 			}
 			
-			for(int j=0;j<mode[cctv_type][i].length;j++){
-				int dir = mode[cctv_type][i][j];
-		
-				int nx = x + dx[dir];
-				int ny = y + dy[dir];
-				while (true) {
-					if(nx < 0 || nx >= n || ny < 0 || ny >= m) {
-						break;
-					}
+			for(int j=0; j<mode[type][i].length; j++) {
+				int d = mode[type][i][j];
+				
+				int nx = x+dx[d];
+				int ny = y+dy[d];
+				
+				while(true) {
+					if(nx<0 || nx>=N || ny<0 || ny>=M) break;
 					
-					if(map[nx][ny] == 6) {
-						break;
-					}
+					if(map[nx][ny] == 6) break;
+					
 					map_copy[nx][ny] = -1;
-					nx += dx[dir];
-					ny += dy[dir];
+					nx += dx[d];
+					ny += dy[d];
 				}
 			}
-
 			
-			combination(depth+1, r, map_copy);
+			comb(depth+1, size, map_copy);
 		}
 	}
 	
 	public static int check(int[][] map) {
 		int cnt = 0;
-		for(int i=0;i<n;i++) {
-			for(int j=0;j<m;j++) {
+		for(int i=0; i<N; i++) {
+			for(int j=0; j<M; j++) {
 				if(map[i][j] == 0) {
 					cnt++;
 				}
@@ -106,7 +98,15 @@ public class Main {
 		return cnt;
 	}
 	
-	
-	
-
+	public static class Pos {
+		int type;
+		int x;
+		int y;
+		
+		public Pos(int type, int x, int y){
+			this.type = type;
+			this.x = x;
+			this.y = y;
+		}
+	}
 }
