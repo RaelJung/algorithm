c3ae84061bc9e1a58c5aa3e7ac57724825d1d029
Rael <allice2283006@gmail.com>
2023-08-22 15:51:41 +0900

    [D5] Title: [S/W 문제해결 응용] 3일차 - 최적 경로, Time: 329 ms, Memory: 21,096 KB -BaekjoonHub

--- SWEA/D5/1247. ［S／W 문제해결 응용］ 3일차 － 최적 경로/［S／W 문제해결 응용］ 3일차 － 최적 경로.java
+++ SWEA/D5/1247. ［S／W 문제해결 응용］ 3일차 － 최적 경로/［S／W 문제해결 응용］ 3일차 － 최적 경로.java
@@ -2,94 +2,82 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-//NP
+//기본적인 순열
 public class Solution {
 	static int T, N, comY, comX, homeY, homeX, min;
-	static int cust[][];
-	static int index[];		//np를 위한 변수
-	
-	static StringBuilder sb = new StringBuilder();
-	
-	public static void main(String[] args) throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		T = Integer.parseInt(br.readLine());
-		
-		for(int t=1; t<=T; t++) {
-			N = Integer.parseInt(br.readLine());
-			cust= new int[N][2];
-			
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			comY = Integer.parseInt(st.nextToken());
-			comX = Integer.parseInt(st.nextToken());
-			homeY = Integer.parseInt(st.nextToken());
-			homeX = Integer.parseInt(st.nextToken());
-			
-			for(int i=0; i<N; i++) {
-				cust[i][0] = Integer.parseInt(st.nextToken());
-				cust[i][1] = Integer.parseInt(st.nextToken());
-			}
-			
-			//초기화
-			min = Integer.MAX_VALUE;
-			
-			//풀이
-			index = new int[N];
-			for(int i=0; i<N; i++) {
-				index[i] = i;
-			}
-			
-			while(true) {
-				//complete code
-				check();
-				
-				if(! np(index)) break;
-			}
-			
-			sb.append("#"+t+" "+min+"\n");
-		}
-		System.out.println(sb);
-	}
-	
-	static void check() {
-		//회사 -> 첫번째 고객
-		int sum = distance(comY, comX, cust[index[0]][0], cust[index[0]][1]);
-		//첫번째 고객 -> 마지막 고객
-		for (int i = 0; i < N-1; i++) {
-			sum += distance(
-					cust[index[i]][0], cust[index[i]][1],
-					cust[index[i+1]][0], cust[index[i+1]][1]
-					);
-		}
-		//마지막 고객 -> 집
-		sum += distance(homeY, homeX, cust[index[N-1]][0], cust[index[N-1]][1]);
-		min = Math.min(min, sum);
-	}
-	
-	static int distance(int y1, int x1, int y2, int x2) {
-		return Math.abs(y1 - y2) + Math.abs(x1 - x2); 
-	}
-	
-	static boolean np(int array[]) {
-		int i,j,k;
-		
-		i = j = k = array.length-1;
-		
-        while(i > 0 && array[i-1] >= array[i]) i--;
-        if(i == 0) return false;
+    static int[][] cust;
+    static int[] tgt; // cust -> tgt 순열 채우고 처리
+    static boolean[] select;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        T = Integer.parseInt(br.readLine());
         
-        while(array[i-1] >= array[j] ) j--;
-        
-        swap(array, i-1, j);
-        
-        while(k > i) {
-            swap(array, i++, k--);
+        for (int t = 1; t <= T; t++) {
+            
+            N = Integer.parseInt(br.readLine());
+            cust = new int[N][2];
+            select = new boolean[N];
+            tgt = new int[N];
+            
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            comY = Integer.parseInt(st.nextToken());
+            comX = Integer.parseInt(st.nextToken());
+            homeY = Integer.parseInt(st.nextToken());
+            homeX = Integer.parseInt(st.nextToken());
+            
+            for (int i = 0; i < N; i++) {
+                cust[i][0] = Integer.parseInt(st.nextToken());
+                cust[i][1] = Integer.parseInt(st.nextToken());
+            }
+            
+            // 초기화
+            min = Integer.MAX_VALUE;
+            
+            // 풀이
+            perm(0, 0);
+            
+            sb.append("#").append(t).append(" ").append(min).append("\n");
         }
-        return true;
+        System.out.println(sb);
+    }
+
+    static void perm(int tgtIdx, int sum) { // sum 현재까지 누적거리의 합
+        // 기저조건
+        if( tgtIdx == N ) { // 마지막 고객만 추가로 계산해서 처리
+            // complete code
+            // 마지막 고객 -> 집
+            sum += distance(homeY, homeX, cust[tgt[N-1]][0], cust[tgt[N-1]][1]);
+            min = Math.min(min, sum);
+            
+            return;
+        }
+        
+        for (int i = 0; i < N; i++) {
+            if( select[i] ) continue;
+            
+            tgt[tgtIdx] = i;
+            
+            select[i] = true;
+            
+            int dis = 0; // 거리 계산
+            if( tgtIdx == 0 ) {
+                dis = distance(comY, comX, cust[tgt[0]][0], cust[tgt[0]][1]);
+            }else {
+                dis = distance( 
+                        cust[tgt[tgtIdx-1]][0], cust[tgt[tgtIdx-1]][1],
+                        cust[tgt[tgtIdx]][0], cust[tgt[tgtIdx]][1]
+                        );
+            }
+            // 가지치기
+            if( sum + dis < min )
+                perm(tgtIdx + 1, sum + dis);
+            
+            select[i] = false;
+        }
     }
     
-    static void swap(int[] arr, int i, int j) {
-        int tmp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = tmp;
+    static int distance(int y1, int x1, int y2, int x2) {
+        return Math.abs(y1 - y2) + Math.abs(x1 - x2);
     }
 }
