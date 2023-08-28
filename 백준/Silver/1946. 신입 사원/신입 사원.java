fdb3565b5da33353774c92bc1dea490669167661
Rael <allice2283006@gmail.com>
2023-08-29 03:08:33 +0900

    [Silver I] Title: 신입 사원, Time: 3284 ms, Memory: 311312 KB -BaekjoonHub

---
+++ 백준/Silver/1946. 신입 사원/신입 사원.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for(int i=0; i<n; i++){
+            int m = sc.nextInt();
+            int count = 1;
+            int work[]=new int[m+1];
+            for(int j=0; j<m; j++){
+                int a = sc.nextInt();
+                int b = sc.nextInt();
+                work[a] = b;
+            }
+
+            int vot = work[1];
+
+            for(int j=2; j<=m; j++){
+                if(work[j]<vot){
+                    vot = work[j];
+                    count++;
+
+                }
+            }
+            System.out.println(count);
+        }
+        sc.close();
+    }
+}
