42867b5ebd985ed4e84ffa8226a6b2b337e2b6d3
Rael <allice2283006@gmail.com>
2023-08-29 17:29:57 +0900

    [Gold V] Title: 평범한 배낭, Time: 156 ms, Memory: 14672 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -4,7 +4,7 @@ import java.util.StringTokenizer;
 
 public class Main {
 	static int N, K;
-	static int memoi[][];
+	static int memoi[];
 	static int item[][];
 	
 	public static void main(String[] args) throws Exception{
@@ -15,7 +15,7 @@ public class Main {
 		K = Integer.parseInt(st.nextToken());
 		
 		//memoi 초기화
-		memoi = new int[N+1][K+1];		//0은 더미
+		memoi = new int[K+1];		//0은 더미
 		
 		//item 세팅
 		item = new int[N+1][2];			//0은 더미
@@ -28,24 +28,18 @@ public class Main {
 		//가치 합의 최댓값
 		//item
 		for(int i=1; i<=N; i++) {
-			for(int w=1; w<=K; w++) {
-				//현재 item이 현재 무게에 선택 가능?
-				if(item[i][0] <= w) {
-					//선택 O
-					//memoi[i-1][w] => i번째 item을 고려하기 전까지 w 무게를 만드는데 최상의 value
-					int sel_o = memoi[i-1][w - item[i][0]] + item[i][1];
-					
-					//선택X
-					int sel_x = memoi[i-1][w];
-					memoi[i][w] = Math.max(sel_o, sel_x);
-				}
-				else {
-					memoi[i][w] = memoi[i-1][w];
-				}
+			int w = item[i][0];
+			int v = item[i][1];
+			
+			//큰 무게부터 작은 무게로 옮겨가기
+			for(int k=K; k>=w; k--) {
+				//memoi[w] => i번째 item을 고려하기 전까지 w 무게를 만드는데 최상의 value
+				//memoi[w]를 새로운 현재 고려하는 값으로 덮어쓰지 않으면 이전 item을 고려한 최상의 value
+				memoi[k] = Math.max(memoi[k], memoi[k-w]+v);
 			}
 			
 		}
 		
-		System.out.println(memoi[N][K]);
+		System.out.println(memoi[K]);
 	}
 }
