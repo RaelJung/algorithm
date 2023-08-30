33e8596f6494926542c70dc468ec85804983edeb
Rael <allice2283006@gmail.com>
2023-08-30 12:39:27 +0900

    [Silver V] Title: 다리 놓기, Time: 164 ms, Memory: 15148 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.java
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -15,20 +15,22 @@ public class Main {
 			N = Integer.parseInt(st.nextToken());
 			M = Integer.parseInt(st.nextToken());
 			
-			memoi = new int[M+1][N+1];
-			for(int i=1; i<=M; i++) {
-				//nC0이 되어야 하므로 j=0부터 시작
-				for(int j=0; j<=N; j++) {
-					if(i==0 || j==0 || i==j) {
-						memoi[i][j] = 1;
-					}
-					else {
-						memoi[i][j] = memoi[i-1][j-1] + memoi[i-1][j];
-					}
-				}
-			}
+			memoi = new int[M+1][N+1];		//0은 더미
+			
+			comb(M,N);
 			
 			System.out.println(memoi[M][N]);
 		}
+	}
+	
+	public static int comb(int srcIdx, int tgtIdx) {
+		if(srcIdx == tgtIdx || tgtIdx==0) {
+			return memoi[srcIdx][tgtIdx] = 1;
+		}
+		
+		//이미 계산했으면 그걸 이용
+		if(memoi[srcIdx][tgtIdx] > 0) return memoi[srcIdx][tgtIdx];
+		//아직 계산된 값이 없으면 선택, 비선택 두 가지 경우의 수를 모두 구한 뒤에 합친다.
+		return memoi[srcIdx][tgtIdx] = comb(srcIdx-1, tgtIdx-1) + comb(srcIdx-1, tgtIdx);
 	}
 }
