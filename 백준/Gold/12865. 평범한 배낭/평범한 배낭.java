a1df1461a54e3ebbce60900f59d1bf9645139556
Rael <allice2283006@gmail.com>
2023-09-06 19:47:18 +0900

    [Gold V] Title: 평범한 배낭, Time: 152 ms, Memory: 14684 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -15,10 +15,10 @@ public class Main {
 		K = Integer.parseInt(st.nextToken());
 		
 		//memoi 초기화
-		memoi = new int[K+1];		//0은 더미
+		memoi = new int[K+1];		//0 dummy
 		
 		//item 세팅
-		item = new int[N+1][2];			//0은 더미
+		item = new int[N+1][2];			//0 dummy
 		for(int i=1; i<=N; i++) {
 			st = new StringTokenizer(br.readLine());
 			item[i][0] = Integer.parseInt(st.nextToken());
@@ -26,15 +26,13 @@ public class Main {
 		}
 		
 		//가치 합의 최댓값
-		//item
 		for(int i=1; i<=N; i++) {
 			int w = item[i][0];
 			int v = item[i][1];
 			
 			//큰 무게부터 작은 무게로 옮겨가기
 			for(int k=K; k>=w; k--) {
-				//memoi[w] => i번째 item을 고려하기 전까지 w 무게를 만드는데 최상의 value
-				//memoi[w]를 새로운 현재 고려하는 값으로 덮어쓰지 않으면 이전 item을 고려한 최상의 value
+				//memoi[w] => 이전까지의 최선의 값이므로 이번 것과 비교해서 업데이트
 				memoi[k] = Math.max(memoi[k], memoi[k-w]+v);
 			}
 			
