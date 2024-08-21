a2809b9943568b9dd88175a31ea5917d940581af
Rael <allice2283006@gmail.com>
2024-08-21 19:26:44 +0900

    [Gold IV] Title: 알파벳, Time: 904 ms, Memory: 18196 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -1,55 +1,48 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class Main {
-
-	static int R, C;
-	static int[][] map;
-	static boolean[] visit = new boolean[26];
-	static int[] dx = { -1, 1, 0, 0 };
-	static int[] dy = { 0, 0, -1, 1 };
-	static int ans = 0;
-
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-
-		R = Integer.parseInt(st.nextToken());
-		C = Integer.parseInt(st.nextToken());
-
-		map = new int[R][C];
-		for (int i = 0; i < R; i++) {
-			String str = br.readLine();
-			for (int j = 0; j < C; j++) {
-				map[i][j] = str.charAt(j) - 'A';
-			}
-		}
-
-		dfs(0, 0, 0);
-
-		System.out.println(ans);
-	}
-
-    public static void dfs(int x, int y, int count) {
-        //기저조건
-		if (visit[map[x][y]]) { 
-			ans = Math.max(ans, count);
-			return;
-		} 
-
-        //풀이
-        visit[map[x][y]] = true;
-        for (int i = 0; i < 4; i++) {
-            int cx = x + dx[i];
-            int cy = y + dy[i];
-
-            if (cx >= 0 && cy >= 0 && cx < R && cy < C) {
-                dfs(cx, cy, count + 1);
-            }
-
+    static int R, C;
+    static char[][] board;
+    static boolean[] visited = new boolean[26]; // 알파벳 방문 여부 체크
+    static int maxCount = 0;
+    
+    // 상하좌우 이동을 위한 방향 벡터
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        R = sc.nextInt();
+        C = sc.nextInt();
+        board = new char[R][C];
+        
+        for (int i = 0; i < R; i++) {
+            board[i] = sc.next().toCharArray();
         }
-
-        visit[map[x][y]] = false;   //원복
-	}
+        
+        // 첫 번째 칸의 알파벳 방문 처리 후 DFS 시작
+        visited[board[0][0] - 'A'] = true;
+        dfs(0, 0, 1);
+        
+        System.out.println(maxCount);
+    }
+    
+    static void dfs(int x, int y, int count) {
+        maxCount = Math.max(maxCount, count);
+        
+        // 상하좌우로 이동
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            
+            if (nx >= 0 && ny >= 0 && nx < R && ny < C) { // 보드 범위 내인지 확인
+                int index = board[nx][ny] - 'A';
+                if (!visited[index]) { // 아직 방문하지 않은 알파벳이라면
+                    visited[index] = true;
+                    dfs(nx, ny, count + 1);
+                    visited[index] = false; // 백트래킹
+                }
+            }
+        }
+    }
 }
