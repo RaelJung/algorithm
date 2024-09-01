f6f1b8f46541a5ba7469bc90e1f1414ee8de3e5c
Rael <allice2283006@gmail.com>
2024-09-01 19:31:41 +0900

    [level 1] Title: K번째수, Time: 0.75 ms, Memory: 83.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+        
+        for(int i=0; i<commands.length; i++){
+            int[] arr = new int[(commands[i][1] - (commands[i][0]-1))];
+                for(int j=commands[i][0]-1; j<commands[i][1]; j++){
+                    arr[j-(commands[i][0]-1)] = array[j];
+                }
+            Arrays.sort(arr);
+
+            System.out.println();
+            answer[i] = arr[commands[i][2]-1];
+        }
+        
+        return answer;
+    }
+}
