aa13bfa5361ed363dc819d2ff215c925c4ff9317
Rael <allice2283006@gmail.com>
2024-09-05 15:59:13 +0900

    [level 3] Title: 단어 변환, Time: 2.86 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,82 +1,72 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 class Solution {
-    static String word[], t;
-    static boolean visit[];
-    static int answer=Integer.MAX_VALUE, len=0;
+    static int answer = Integer.MAX_VALUE;
+    static String t;
+    static boolean[] visit;
+    static String[] word;
     
     public int solution(String begin, String target, String[] words) {
-        
-        word = words;
-        len = word.length;
         t = target;
-        visit = new boolean[len];
+        word = words;
+        visit = new boolean[words.length];
+        bfs(new Word(begin, 0));
         
-        bfs(begin, 0);
-        
+        // 변환 불가시 0 return
         if(answer == Integer.MAX_VALUE)
             answer = 0;
         
         return answer;
     }
     
-    static void bfs(String begin, int depth){
-        // Word 큐 정의
+    public static void bfs(Word start){
         Queue<Word> q = new LinkedList<>();
-        q.add(new Word(begin, depth, -1));  //idx가 없으므로 -1
-        
+        q.add(start);
         
         while(!q.isEmpty()){
-            // 큐 꺼내서(방문 체크), 
-            // 하나 다른 애들 있는지 확인 후 추가(depth+1)
             Word curr = q.poll();
-            if(curr.idx != -1)
-                visit[curr.idx] = true;
             
-            // 종료 조건
-            if(t.equals(curr.word)){
-                if(curr.depth < answer){
-                    answer = curr.depth;
-                }
+            // 큐에서 뽑아서 타겟과 비교. 
+            // 이 때, 변환 횟수를 최소로 변환
+            if(curr.word.equals(t)){
+                if(answer > curr.depth) answer = curr.depth;
                 return;
             }
             
-            for(int i=0; i<len; i++){
+            for(int i=0; i<word.length; i++){
+                // 기준 단어와 1개만 다른 단어를 모두 큐에 넣기
+                // depth 추가!
                 if(!visit[i] && isOneDiff(curr.word, word[i])){
-                    q.add(new Word(word[i], curr.depth+1, i));
+                    visit[i] = true;;
+                    q.add(new Word(word[i], curr.depth+1));
                 }
-            }   
+            }
         }
-        
     }
     
-    // 하나만 다른 단어인지 확인
-    static boolean isOneDiff(String w1, String w2){
+    // 하나의 문자가 다른지 확인하는 함수
+    public static Boolean isOneDiff(String w1, String w2){
         int cnt = 0;
         
         for(int i=0; i<w1.length(); i++){
-            // 다르면 카운트 증가
-            if(w1.charAt(i) != w2.charAt(i)){
+            if(w1.charAt(i) != w2.charAt(i))
                 cnt++;
-            }
         }
         
-        if(cnt == 1)
+        if(cnt == 1) {
             return true;
+        }
         return false;
     }
     
-    // 큐 클래스 정의
     public static class Word{
         String word;
         int depth;
-        int idx;
         
-        public Word(String word, int depth, int idx) {
+        public Word(String word, int depth){
             this.word = word;
             this.depth = depth;
-            this.idx = idx;
         }
     }
 }
