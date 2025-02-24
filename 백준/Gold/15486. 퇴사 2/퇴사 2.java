2609bb3fe5cf147cb31cddf49d204e35f8dbd5a0
Rael <allice2283006@gmail.com>
2025-02-24 20:17:45 +0900

    [Gold V] Title: 퇴사 2, Time: 664 ms, Memory: 316412 KB -BaekjoonHub

--- 백준/Gold/15486. 퇴사 2/퇴사 2.java
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.java
@@ -1,30 +1,42 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    static int N, max_profit;
+    static int[] T, P, dp;
 
-        int N = scanner.nextInt();
-        int[] T = new int[N + 1];
-        int[] P = new int[N + 1];
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        for (int i = 1; i <= N; i++) {
-            T[i] = scanner.nextInt();
-            P[i] = scanner.nextInt();
+        N = Integer.parseInt(st.nextToken());
+        T = new int[N+1];
+        P = new int[N+1];
+        dp = new int[N+2]; //N일까지 일하므로
+
+        dp[0] = 0; //초기값은 0 
+        max_profit = 0;
+        for(int i=1; i<N+1; i++){
+            st = new StringTokenizer(br.readLine());
+            T[i] = Integer.parseInt(st.nextToken());
+            P[i] = Integer.parseInt(st.nextToken());
+
+            dp[i] = Math.max(max_profit, dp[i]);    //상담을 안하면 이전의 최댓값 적용
+            if(i+T[i]<=N+1) //수익은 N+1에 적용될 수 O
+                dp[i+T[i]] = Math.max(dp[i]+P[i], dp[i+T[i]]);  //상담을 결정 했다면 해당 시간에 금액 더해줌 (그리고 기존 값이랑 비교해 최댓값 넣어주기)
+            max_profit = Math.max(max_profit, dp[i]); // 최댓값 업데이트
         }
 
-        int[] dp = new int[N + 2]; // N+1까지 사용하기 위해 N+2 크기로 선언
-
-        for (int i = N; i > 0; i--) {
-            if (i + T[i] <= N + 1) {
-                dp[i] = Math.max(dp[i + 1], P[i] + dp[i + T[i]]);
-            } else {
-                dp[i] = dp[i + 1];
-            }
-        }
-
-        System.out.println(dp[1]);
-
-        scanner.close();
+        System.out.println(Math.max(max_profit, dp[N+1])); // 최종 최댓값 출력
     }
 }
+
+// DP에도 2가지 종류가 있군아...
+// 1. 과거의 값이 현재의 값에 영향 O
+// ex) 최대 이익 스케쥴, 배낭 문제, 최대 점수 등
+// => 이 경우 max_profit으로 언제나 최댓값을 보장해야 함
+// --------------------------------------
+// 2. 과거의 값이 현재의 값에 영향 X
+// ex) 최소 연산 횟수, 계단 오르기, 피보나치 수열
+// => 이 경우 최댓값 보장필요 X (독립 시행에 해당하므로)
