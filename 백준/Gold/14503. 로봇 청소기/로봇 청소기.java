b424f8800f25a29dc524750c2d4bc85857733a41
Rael <allice2283006@gmail.com>
2025-05-10 13:17:16 +0900

    [Gold V] Title: 로봇 청소기, Time: 108 ms, Memory: 14436 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.java
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.java
@@ -1,70 +1,68 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
- 
+import java.io.*;
+import java.util.*;
+
 public class Main {
-	// BOJ / 14503번 / 로봇청소기 / 그래프 / 골5
-	
-	static int y,x,count;
-	static int map[][];
-	static int dy[] = {-1,0,1,0};  // 북동남서
-	static int dx[] = {0,1,0,-1};
-	public static void main(String[] args) throws IOException {
- 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer stk = new StringTokenizer(br.readLine(), " ");
-		
-		y = Integer.parseInt(stk.nextToken()); 
-		x = Integer.parseInt(stk.nextToken());
-		map = new int[y][x];
-		
-		stk = new StringTokenizer(br.readLine(), " ");
-		int r = Integer.parseInt(stk.nextToken());
-		int c = Integer.parseInt(stk.nextToken());
-		int d = Integer.parseInt(stk.nextToken()); // 방향 0북 , 1동, 2남, 3서
-		count = 1;
-		
-		for(int i=0; i<y; i++) {
-			stk = new StringTokenizer(br.readLine(), " ");
-			for(int j=0; j<x; j++) {
-				map[i][j] = Integer.parseInt(stk.nextToken());
-			}
-		}
- 
-		dfs(r,c,d);
-		System.out.println(count);
-	}
-	
-	public static void dfs(int r, int c, int direction) {
-		
-		//현재 위치를 청소한다.
-		map[r][c] = -1;
-		
-		//현재 위치에서 현재 방향을 기준으로 왼쪽방향부터 차례대로 탐색을 진행한다.
-		for(int i=0; i<4; i++) {
-			direction = (direction + 3) % 4; // 왼쪽 방향으로 변환 (북 -> 서 -> 남 -> 동)
-			int ny = r + dy[direction];
-			int nx = c + dx[direction];
-			
-			// 청소가 안된 곳이 있으면 count++,  dfs
-			if(ny >=0 && ny < y && nx >= 0 && nx < x && map[ny][nx] == 0) {
-				count++;
-				dfs(ny,nx, direction);
-				
-				//여기서 return을 안하면 복귀하는 도중 뒤로가서 다른 곳을 청소할수가 있음.
-				return;
-			}
-		}
-		
-		//네 방향 모두 청소가 이미 되어있거나 벽인 경우에는
-		//뒤쪽 칸이 벽이 아니라는 전제 하에, 바라보는 방향을 유지한 채로 한 칸 후진.
-		int back = (direction + 2) % 4;
-		int by = r + dy[back];
-		int bx = c + dx[back];
-        
-		if(by>=0 && by<y && bx>=0 && bx<x && map[by][bx] != 1) {
-			dfs(by,bx,direction);
-		}
-	}
+    static int N, M, cnt=0;
+    static int sx, sy;
+    static int[][] map;
+
+    //0:북 1:동 2:남 3:서
+    static int[] dy = {-1, 0, 1, 0};
+    static int[] dx = {0, 1, 0, -1};
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        //시작점 및 방향 d 입력 받기
+        sy = Integer.parseInt(st.nextToken());
+        sx = Integer.parseInt(st.nextToken());
+        int d = Integer.parseInt(st.nextToken());
+
+        map = new int[N][M];
+        for(int i=0; i<N; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j=0; j<M; j++){
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        cleanRoom(sy,sx, d);
+
+        System.out.println(cnt);
+    }
+
+    public static void cleanRoom(int y, int x, int d){
+        //1. 현재 칸 청소
+        if(map[y][x] == 0){
+            map[y][x] = 2;
+            cnt++;
+        }
+    
+        //2. 4방향 탐색
+        for(int i=0; i<4; i++){
+            int nd = (d + 3 - i) % 4; // 반시계 방향으로 탐색
+            int ny = y + dy[nd];
+            int nx = x + dx[nd];
+    
+            if(nx < 0 || ny < 0 || nx >= M || ny >= N) continue;
+            if(map[ny][nx] == 0){ // 미청소 구역 발견
+                cleanRoom(ny, nx, nd);
+                return;
+            }
+        }
+    
+        //3. 네 방향 모두 청소 or 벽일 경우 → 뒤로 이동
+        int back = (d + 2) % 4;
+        int ny = y + dy[back];
+        int nx = x + dx[back];
+    
+        if(nx < 0 || ny < 0 || nx >= M || ny >= N || map[ny][nx] == 1) return; // 벽이면 종료
+        cleanRoom(ny, nx, d); // 방향 유지한 채 뒤로 이동
+    }
+    
 }
