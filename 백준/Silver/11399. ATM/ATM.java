cd80c5440adb5f4450237e50458183107836742e
Rael <allice2283006@gmail.com>
2025-05-11 13:20:26 +0900

    [Silver IV] Title: ATM, Time: 112 ms, Memory: 14428 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.java
+++ 백준/Silver/11399. ATM/ATM.java
@@ -1,29 +1,27 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    static int N, ans, sum;
-    static int[] P;
+    static int N, sum=0, total=0;
+    static int[] time;
 
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        P = new int[N];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
+        time = new int[N];
         for(int i=0; i<N; i++){
-            P[i] = Integer.parseInt(st.nextToken());
+            time[i] = Integer.parseInt(st.nextToken());
         }
 
-        Arrays.sort(P);
+        Arrays.sort(time);
 
-        ans = sum = 0;
         for(int i=0; i<N; i++){
-            sum += P[i];
-            ans += sum;
+            sum += time[i];
+            total += sum;
         }
-        System.out.println(ans);
+
+        System.out.println(total);
     }
 }
