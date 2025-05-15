b82182ae26189150fed94c8499821bbcadfd3395
Rael <allice2283006@gmail.com>
2025-05-15 21:07:49 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 1148 ms, Memory: 62716 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -1,32 +1,43 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	static int nums[];
-	static int N,M;
-	
-	public static void main(String[] args) throws Exception{
-		 BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		 StringTokenizer st = new StringTokenizer(br.readLine());
-		 
-		 N = Integer.parseInt(st.nextToken());
-		 M = Integer.parseInt(st.nextToken());
-		 
-		 st = new StringTokenizer(br.readLine());
-		 nums = new int[N+1];
-		 nums[0] = 0;
-		 
-		 for(int i=1; i<=N; i++) {
-			 nums[i] = Integer.parseInt(st.nextToken()) + nums[i-1];
-		 }
-		 
-		 for(int i=0; i<M; i++) {
-			 st = new StringTokenizer(br.readLine());
-			 int s_idx = Integer.parseInt(st.nextToken());
-			 int e_idx = Integer.parseInt(st.nextToken());
-			 
-			 System.out.println(nums[e_idx]-nums[s_idx-1]);
-		 }
-	}
+    static int N, K;
+    static int num[], sum[];
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        num = new int[N];
+        sum = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<N; i++){
+            num[i] = Integer.parseInt(st.nextToken());
+        }
+
+        sum[0] = num[0];
+        //전체 합부터 먼저 ㄱㄱ 차피 다 더할테니깐요 이건 아까 걔처럼 안 정해져있음 개수가
+        for(int i=1; i<N; i++){
+            sum[i] = sum[i-1]+num[i];
+        }
+
+        for(int i=0; i<K; i++){
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+
+            int total=sum[e-1];
+            if((s-1)>0){
+                total -= sum[s-2];
+            }
+
+            System.out.println(total);
+        }
+
+
+    }
 }
